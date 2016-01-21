96038333332e925a98f8f7d4c1ad9b1099c6d3f6
Lauricio Su <me@lauricio.com>
2016-01-21 13:35:45 +0700

    initial commit

---
+++ README.md
@@ -0,0 +1,27 @@
+# Grid
+[![Meteor Icon](http://icon.meteor.com/package/ox2:grid)](https://atmospherejs.com/ox2/grid)
+
+## Summary
+Adds custom html elements to create layout grids.
+
+## Install
+```
+meteor add ox2:grid
+```
+
+## Changelog
+### v0.0.1 - 16 Jan 2016
+* Initial release.
+
+Read full [CHANGELOG.md](https://github.com/ox2/oo-grid/tree/master/CHANGELOG.md)
+
+## About
+Extended package description with code samples, etc. (optional)
+
+## License
+[MIT](https://github.com/ox2/oo-grid/tree/master/LICENSE)
+
+Copyright (c) 2016 ox2
+
+## API
+Full API docs available [here](http://ox2.github.io/oo-grid)
---
+++ package.js
@@ -0,0 +1,34 @@
+Package.describe({
+  name: 'ox2:grid',
+  version: '0.0.1',
+  // Brief, one-line summary of the package.
+  summary: 'DO NOT USE',
+  // URL to the Git repository containing the source code for this package.
+  git: '',
+  // By default, Meteor will default to using README.md for documentation.
+  // To avoid submitting documentation, set this field to null.
+  documentation: ''
+});
+
+var C = 'client';
+var S = 'server';
+var CS = [C, S];
+
+Package.onUse(function(api) {
+    api.versionsFrom('1.2.1');
+    // Core
+    api.use([
+      'templating'
+    ]);
+    // 3rd party
+    api.use([
+      'lauricio:less-autoprefixer@2.5.0_3'
+    ]);
+    api.addFiles('lib/oo-grid.less', C);
+});
+
+Package.onTest(function(api) {
+  api.use('tinytest');
+  api.use('ox2:grid');
+  api.addFiles('tests/package-tests.js');
+});
